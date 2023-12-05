b1f11e75665bdf3fb04c395406486a8a95f2d55d
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-05 16:49:38 +0000

    updated layout

--- Source/data/employee_data.py
+++ Source/data/employee_data.py
@@ -5,14 +5,17 @@ class Employee_data:
     def __init__(self) -> None:
         self.model = "files/employees.csv"
 
-    def employeeConstructor(self):
+    def employee_constructor(self):
         employee_list = []
         file = csv.DictReader(open(self.model, 'r'))
         for row in file:
             employee_list.append(Employee(row["id"],row["name"],row["address"],row["cell_phone"],row["email"],row["title"],row["home_phone"],row["current_trip"],row["plane_licenses"]))
         return employee_list
-    def addEmployeeData(self):
-        pass
+    def add_employee_data(self,employee):
+        #Employee(id,name,address,cell_phone,email,title,home_phone,current_trip,plane_licenses)
+        file = csv.DictReader(open(self.model, 'w'))
+    def modify_employee_data(self,id):
+        file = csv.DictReader(open(self.model, 'r'))
+        for row in file:
+            if row["id"]==id:
 
-    def modifyEmployeeData(self):
-        pass
