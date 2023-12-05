4d60d5a702d3e3e159271dc8d8f980b870b51ac8
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-05 13:07:55 +0000

    removed filler data from employee

--- Source/model/employee_model.py
+++ Source/model/employee_model.py
@@ -1,9 +1,2 @@
 class Employee:
-    def __init__(self, id, location, Job, plane, startDateHome, startDateReturn, ticketSold) -> None:
-        self.id = id
-        self.location = location
-        self.employee = employee
-        self.plane = plane
-        self.startDateHome = startDateHome
-        self.startDateReturn = startDateReturn
-        self.ticketSold = ticketSold
+    def __init__(self,) -> None:
