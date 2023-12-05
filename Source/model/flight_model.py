c4e35b136c9e4c1e22c421490c1a8dbc3936c348
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-05 11:02:41 +0000

    added unfinished flight model

---
+++ Source/model/flight_model.py
@@ -0,0 +1,25 @@
+class Flight:
+    def __init__(self, flightId, location, employee, plane, startDateHome, startDateReturn, ticketSold) -> None:
+        self.flightId = flightId
+        self.location = location
+        self.employee = employee
+        self.plane = plane
+        self.startDateHome = startDateHome
+        self.startDateReturn = startDateReturn
+        self.ticketSold = ticketSold
+    def get(self):
+        FlightId
+        Location
+        Employee
+        Plane
+        StartDateHome
+        StartDateReturn
+        TicketSold
+
+        self.flightId = flightId
+        self.location = location
+        self.employee = employee
+        self.plane = plane
+        self.startDateHome = startDateHome
+        self.startDateReturn = startDateReturn
+        self.ticketSold = ticketSold
