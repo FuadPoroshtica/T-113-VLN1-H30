4723767803e78a8c5e782a68841c1858026ab374
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-05 15:13:18 +0000

    Changed employee to make more sense

--- Source/data/employee_data.py
+++ Source/data/employee_data.py
@@ -1,13 +1,18 @@
-class Employee_data():
-    def __init__(self, csv_doc, model) -> None:
-        self.employee = csv_doc
-        self.model = model
+import csv
+from model.employee_model import Employee
+class Employee_data:
+    def __init__(self) -> None:
+        self.model = "files/employee.csv"
     def employeeConstructor(self):
-        for line in self.employee:
-            pass
+        employee_list = []
+        file = csv.DictReader(open(self.employee, 'r'))
+        for line in file:
+            print(line)
     def addEmployeeData(self):
         for line in self.employee:
             pass
     def modifyEmployeeData(self):
         for line in self.employee:
             pass
+a = Employee_data
+a.employeeConstructor()
