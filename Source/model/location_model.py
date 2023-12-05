7edee0de0c6bafd64a7a2cd55045d652716c39ec
Shjellinn <142911104+Shjellinn@users.noreply.github.com>
2023-12-05 12:59:59 +0000

    Added location_model.py

---
+++ Source/model/location_model.py
@@ -0,0 +1,9 @@
+class Location:
+    def __init__(self, location_code_id, country, airport, flight_time, distance_from_iceland, manager_name, emergency_phone ):
+        self.location_code_id = location_code_id
+        self.country = country
+        self.airport = airport
+        self.flight_time = flight_time
+        self.distance_from_iceland = distance_from_iceland
+        self.manager_name = manager_name
+        self.emergency_phone = emergency_phone
