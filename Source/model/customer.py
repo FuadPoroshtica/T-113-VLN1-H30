f7561dd372835a3dcebff2422211b19feb5f9131
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-05 12:50:27 +0000

    small change on employee

--- Source/model/customer.py
+++
@@ -1,7 +0,0 @@
-class Customer:
-    def __init__(self, name="", birth_year="2000"):
-        self.name = name
-        self.birth_year = birth_year
-
-    def __str__(self):
-        return f"name: {self.name}, birth year: {self.birth_year}"
---
+++ Source/model/employee_model.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, id, location, Job, plane, startDateHome, startDateReturn, ticketSold) -> None:
+        self.id = id
+        self.location = location
+        self.employee = employee
+        self.plane = plane
+        self.startDateHome = startDateHome
+        self.startDateReturn = startDateReturn
+        self.ticketSold = ticketSold
