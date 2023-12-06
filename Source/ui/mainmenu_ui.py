8f4cf1090a2fb36026745a9c3f9b7d82cc3f1296
CoolSmurf27 <gardarsolvi@gmail.com>
2023-12-06 12:13:57 +0000

    Update mainmenu_ui.py

--- Source/ui/mainmenu_ui.py
+++ Source/ui/mainmenu_ui.py
@@ -1,13 +1,12 @@
-
-
-#from logic.customer_logic import Customer_Logic
-#from model.customer import Customer
-#from ui.customer_ui import Customer_UI
-#from logic.logic_wrapper import Logic_Wrapper
+# from logic.customer_logic import Customer_Logic
+# from model.customer import Customer
+# from ui.customer_ui import Customer_UI
+# from logic.logic_wrapper import Logic_Wrapper
 
 
 class MainMenu_UI:
     def __init__(self):
+        pass
 
     def menu_output(self):
         print("Main Menu")
@@ -19,6 +18,5 @@ class MainMenu_UI:
         print("6. Logout")
         print("q. Quit")
 
-
     def input_prompt(self):
-      
+        input = input("Select option: ")
