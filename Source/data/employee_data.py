4aaaecf9f1f84365ece3225d106e8165223b88b9
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-06 10:15:32 +0000

    trying to implement adding employees

--- Source/data/employee_data.py
+++ Source/data/employee_data.py
@@ -13,10 +13,17 @@ class Employee_data:
         file.close()
         return employee_list
     def add_employee_data(self,id,name,address,cell_phone,email,title,home_phone="None",current_trip="None",plane_licenses="None"):
-        file_read=csv.DictReader(open(self.model,'r'))
-        column_names=list(dict(list(file_read)[0]))
-        file = csv.DictWriter(open(self.model, 'a'),column_names,lineterminator="\n")
-        file.writerow({"id":id,"name":name,"address":address,"cell_phone":cell_phone,"email":email,"title":title,"home_phone":home_phone,"current_trip":current_trip,"plane_licenses":plane_licenses})
+        with open(self.model,'r') as file_read:
+            dict_read=csv.DictReader(file_read)
+            # dictRead pairs as keys and items, by reading the first line
+            # you get key : item, turning it into 
+            print(list(dict_read))
+            print()
+            column_names=list(dict(list(dict_read)[0]))
+            print(column_names)
+#        file = csv.DictWriter(open(self.model, 'a'),column_names,lineterminator="\n")
+#        file.writerow({"id":id,"name":name,"address":address,"cell_phone":cell_phone,"email":email,"title":title,"home_phone":home_phone,"current_trip":current_trip,"plane_licenses":plane_licenses})
+#        file.close()
     def modify_employee_data(self,id,type):
         file = csv.DictReader(open(self.model, 'rw'))
         for row in file:
