2a4889ff42827cf71c90694ee9deb81737409ccc
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-06 09:50:27 +0000

    added employee creator

--- Source/data/employee_data.py
+++ Source/data/employee_data.py
@@ -10,12 +10,15 @@ class Employee_data:
         file = csv.DictReader(open(self.model, 'r'))
         for row in file:
             employee_list.append(Employee(row["id"],row["name"],row["address"],row["cell_phone"],row["email"],row["title"],row["home_phone"],row["current_trip"],row["plane_licenses"]))
+        file.close()
         return employee_list
-    def add_employee_data(self,employee):
-        #Employee(id,name,address,cell_phone,email,title,home_phone,current_trip,plane_licenses)
-        file = csv.DictReader(open(self.model, 'w'))
-    def modify_employee_data(self,id):
-        file = csv.DictReader(open(self.model, 'r'))
+    def add_employee_data(self,id,name,address,cell_phone,email,title,home_phone="None",current_trip="None",plane_licenses="None"):
+        file_read=csv.DictReader(open(self.model,'r'))
+        column_names=list(dict(list(file_read)[0]))
+        file = csv.DictWriter(open(self.model, 'a'),column_names,lineterminator="\n")
+        file.writerow({"id":id,"name":name,"address":address,"cell_phone":cell_phone,"email":email,"title":title,"home_phone":home_phone,"current_trip":current_trip,"plane_licenses":plane_licenses})
+    def modify_employee_data(self,id,type):
+        file = csv.DictReader(open(self.model, 'rw'))
         for row in file:
             if row["id"]==id:
-
+                pass
