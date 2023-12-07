b4813407e252282459229978ff6b7106bd8191e8
flamingflarion <jonlogidagbjartsson@gmail.com>
2023-12-07 14:09:05 +0000

    added unfinished temp flight logic

--- Source/logic/flight_logic.py
+++ Source/logic/flight_logic.py
@@ -3,3 +3,10 @@ from data.flight_data import flight_data
 class Flight_logic:
     def __init__(self):
         self.location_data = flight_data()
+
+    def get_flights():
+        pass
+    def does_flight_exist():
+        pass
+    def change_specific_employee():
+        pass
