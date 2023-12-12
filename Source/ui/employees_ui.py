9ed4b96673df83fff30591ce1fbe97232fe02b4d
Fuad Poroshtica <Fuadp-01@hotmail.com>
2023-12-12 13:31:46 +0000

    fix,

--- Source/ui/employees_ui.py
+++ Source/ui/employees_ui.py
@@ -2,16 +2,12 @@
 from model.employee_model import Employee
 from logic.logic_wrapper import Logic_Wrapper
 from data.data_wrapper import Data_Wrapper
-<<<<<<< Updated upstream
-from .navigation import return_to_previous_menu, return_to_main_menu, handle_menu_options, menu_stack
-=======
 from .navigation import return_to_previous_menu, return_to_main_menu, menu_stack
-from ui.interface_ui import print_boxed
->>>>>>> Stashed changes
 
 # Initialize Data_Wrapper and LogicWrapper
 data_wrapper = Data_Wrapper()
 logic_wrapper = Logic_Wrapper(data_wrapper)
+
 
 def employees_menu():
     menu_stack.append(employees_menu)
@@ -22,7 +18,6 @@ def employees_menu():
         print("2. Add employees")
         print("3. Modify employees")
         print("Main Menu (M), Back (B), Quit (Q)")
-
         choice = input("Select Option: ").upper()
 
         if choice == "1":
@@ -43,21 +38,32 @@ def employees_menu():
         else:
             print("Invalid choice. Please choose again.")
 
+
 def view_all_employees():
     menu_stack.append(view_all_employees)
     while True:
         all_employees = logic_wrapper.get_all_employees()
-        all_pilots = logic_wrapper.get_pilots()
-        all_cabin_crew = logic_wrapper.get_cabin_crew()
         print("\nList of All Employees:")
         print("----------------------")
-        print(f"{'ID':<12}{'Name':<30}{'Title':<12}{'Home Address':<20}{'Cell Phone':<11}{'Email':<30}{' Home Phone':<11}{'Current Trip':<12}")
-        for employee in all_pilots:
-            print(f"{employee.id:<12}{employee.name:<30}{employee.title:<12}{employee.address:<20}{employee.cell_phone:<11}{employee.email:<30}{employee.home_phone:<11}{employee.current_trip:<5}")
-        for employee in all_cabin_crew:
-            print(f"{employee.id:<12}{employee.name:<30}{employee.title:<12}{employee.address:<20}{employee.cell_phone:<11}{employee.email:<30}{employee.home_phone:<11}{employee.current_trip:<5}")
-        
-        handle_menu_options()
+        for employee in all_employees:
+            print(
+                f"ID: {employee.id}, Name: {employee.name}, Title: {employee.title}, Address: {employee.address}, Cell Phone: {employee.cell_phone}, Email: {employee.email}, Home Phone: {employee.home_phone}, Current Trip: {employee.current_trip}, Plane Licenses: {employee.plane_licenses}"
+            )
+
+        print("\nMain Menu (M), Back (B), Quit (Q)")
+        choice = input("Select Option: ").upper()
+
+        if choice == "M":
+            return_to_main_menu()
+            break
+        elif choice == "B":
+            return_to_previous_menu()
+            break
+        elif choice == "Q":
+            print("Exiting the program.")
+            exit()
+        else:
+            print("Invalid choice. Please choose again.")
 
 
 def add_employees():
