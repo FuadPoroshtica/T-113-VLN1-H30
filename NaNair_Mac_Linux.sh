5914418d3176b41a0b393563298df60dc201171d
Fuad Poroshtica <fuad23@ru.is>
2023-12-15 17:57:13 +0000

    readme

--- NaNair_Mac_Linux.sh
+++ NaNair_Mac_Linux.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Check for Python
-if command -v python3 &>/dev/null; then
-    PYTHON_CMD=python3
-elif command -v python &>/dev/null; then
-    PYTHON_CMD=python
-else
-    echo "Python is not installed. Please install Python and retry."
-    exit 1
-fi
-
-echo "Python is installed, proceeding..."
 echo "Installing requirements..."
-$PYTHON_CMD -m pip install -r Source/requirements.txt
+python -m pip install -r Source/requirements.txt
 
 echo "Running the application..."
-$PYTHON_CMD Source/main.py
+python Source/main.py
