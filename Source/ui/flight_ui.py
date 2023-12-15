2f0774dcb29343794ddce3b22f6b3399e81da3b3
Fuad Poroshtica <fuad23@ru.is>
2023-12-15 15:43:19 +0000

    UI Final Part 1

--- Source/ui/flight_ui.py
+++ Source/ui/flight_ui.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from .navigation import return_to_previous_menu, return_to_main_menu, handle_menu_options, menu_stack
 from logic.logic_wrapper import Logic_Wrapper
 from data.data_wrapper import Data_Wrapper
+from ui.interface_ui import interface
 
 data_wrapper = Data_Wrapper()
 logic_wrapper = Logic_Wrapper(data_wrapper)
@@ -11,13 +12,17 @@ logic_wrapper = Logic_Wrapper(data_wrapper)
 def flights_menu():
     menu_stack.append(flights_menu)
     while True:
-        print("\nFlights Menu")
-        print("---------------")
-        print("1. View Flights")
-        print("2. Create New Flight")
-        print("3. View Flights by Date")
-        print("4. View Flights by Week")
-        print("Main Menu (M), Back (B), Quit (Q)")
+        content = [
+            "Flights Menu",
+            "---------------------------------------------",
+            "1. View Flights",
+            "2. Create New Flight",
+            "3. View Flights by Date",
+            "4. View Flights by Week",
+            "---------------------------------------------",
+            "Main Menu (M), Back (B), Quit (Q)",
+        ]
+        interface(content)
         choice = input("Select Option: ").upper()
 
         if choice == '1':
@@ -35,21 +40,31 @@ def flights_menu():
             return_to_previous_menu()
             break
         elif choice == 'Q':
-            print("Exiting the program.")
+            interface(["Exiting the program."])
             exit()
         else:
-            print("Invalid choice. Please choose again.")
+            interface(["Invalid choice. Please choose again."])
+
 
 def view_flights():
     menu_stack.append(view_flights)
     while True:
         all_flights = logic_wrapper.get_all_flights()
-        print("\nList of All Flights:")
-        print("----------------------")
-        for flight in all_flights:
-            print(f"{flight.id}: From {flight.initial_location} to {flight.arrival_location}, Plane: {flight.plane}, Start: {flight.start_home}, Tickets: Home - {flight.tickets_home}, Foreign - {flight.tickets_foreign}")
 
+        # Building the content list for interface
+        flights_content = ["List of All Flights:", "----------------------"]
+        if all_flights:
+            flights_content.extend(
+                [
+                    f"{flight.id}: From {flight.initial_location} to {flight.arrival_location}, Plane: {flight.plane}, Start: {flight.start_home}, Tickets: Home - {flight.tickets_home}, Foreign - {flight.tickets_foreign}"
+                    for flight in all_flights]
+            )
+        else:
+            flights_content.append("No flights available.")
+
+        interface(flights_content)
         handle_menu_options()
+
 
 def input_with_retry(prompt, validation_func):
     while True:
@@ -58,13 +73,15 @@ def input_with_retry(prompt, validation_func):
             return input_value
         retry = input("Error, input not valid. Would you like to try again? (y/n): ").lower()
         if retry == 'n':
-            return "exit"  # Return a special value to indicate exit
+            return "exit"
 
 def input_ticket_count(prompt, max_capacity):
     while True:
-        input_str = input(prompt)
+        print([prompt])
+        input_str = input("Type here: ")
+
         if not input_str.isdigit():
-            print("Invalid input, please enter a valid integer number.")
+            interface(["Invalid input, please enter a valid integer number."])
             retry = input("Would you like to try again? (y/n): ").lower()
             if retry == 'n':
                 return "exit"  # Return a special value to indicate exit
@@ -72,21 +89,26 @@ def input_ticket_count(prompt, max_capacity):
 
         tickets = int(input_str)
         if tickets > max_capacity:
-            print(f"Error: Ticket count exceeds the maximum seat capacity of {max_capacity}.")
+            interface([f"Error: Ticket count exceeds the maximum seat capacity of {max_capacity}."])
             retry = input("Would you like to try again? (y/n): ").lower()
             if retry == 'n':
                 return "exit"
         else:
             return tickets
 
-def create_flight():
-    print("\nCreate a New Flight")
 
-    initial_airport_code = input_with_retry("Enter Initial Airport Code: ", lambda code: logic_wrapper.location_logic.get_location_by_airport_code(code))
+def create_flight():
+    interface(["Create a New Flight"])
+
+    initial_airport_code = input_with_retry("Enter Initial Airport Code: ",
+                                            lambda code: logic_wrapper.location_logic.get_location_by_airport_code(
+                                                code))
     if initial_airport_code == "exit":
         return
 
-    arrival_airport_code = input_with_retry("Enter Arrival Airport Code: ", lambda code: logic_wrapper.location_logic.get_location_by_airport_code(code))
+    arrival_airport_code = input_with_retry("Enter Arrival Airport Code: ",
+                                            lambda code: logic_wrapper.location_logic.get_location_by_airport_code(
+                                                code))
     if arrival_airport_code == "exit":
         return
 
@@ -94,8 +116,10 @@ def create_flight():
     if plane_id == "exit":
         return
 
-    start_date = input("Enter Start Date (YYYY-MM-DD): ")
-    start_time = input("Enter Start Time at Home Location (HH:MM): ")
+    print("Enter Start Date (YYYY-MM-DD):")
+    start_date = input("Type here: ")
+    print("Enter Start Time at Home Location (HH:MM):")
+    start_time = input("Type here: ")
     start_home = f"{start_date} {start_time}"
 
     plane = logic_wrapper.plane_logic.get_plane_by_id(plane_id)
@@ -108,13 +132,14 @@ def create_flight():
     if tickets_foreign == "exit":
         return
 
-    valid, message = logic_wrapper.validate_flight_creation(plane_id, start_home, arrival_airport_code, tickets_home, tickets_foreign)
+    valid, message = logic_wrapper.validate_flight_creation(plane_id, start_home, arrival_airport_code, tickets_home,
+                                                            tickets_foreign)
     if not valid:
-        print(f"Error: {message}")
+        interface([f"Error: {message}"])
         return
 
     flight_id = logic_wrapper.flight_logic.generate_flight_id(plane, arrival_airport_code, start_home)
-    
+
     flight_data = {
         'id': flight_id,
         'initial_location': initial_airport_code,
@@ -127,16 +152,41 @@ def create_flight():
     }
 
     logic_wrapper.add_flight(flight_data)
-    print("Flight added successfully.")
+    interface(["Flight added successfully."])
+
 
 def view_flights_by_date():
-    date = input("Enter Date (YYYY-MM-DD): ")
-    flights = logic_wrapper.get_flights_by_date_with_manning_info(date)
+    interface(["Enter Date (YYYY-MM-DD): "])
+    start_date = input("Type here: ").strip()
+
+    if not start_date:
+        return flights_menu()
+
+    flights = logic_wrapper.get_flights_by_week_with_manning_info(start_date)
+
+    flight_content = ["Flights for the week starting from " + start_date]
     for flight, is_manned in flights:
-        print(f"Flight {flight.id} on {flight.start_home}: {'Properly Manned' if is_manned else 'Not Properly Manned'}")
+        flight_content.append(
+            f"Flight {flight.id} on {flight.start_home}: {'Properly Manned' if is_manned else 'Not Properly Manned'}")
+
+    flight_content.append("\nPress Enter to return to the Flights Menu.")
+    interface(flight_content)
+
+    input()
+    return flights_menu()
+
 
 def view_flights_by_week():
-    start_date = input("Enter Start Date of Week (YYYY-MM-DD): ")
+    interface(["Enter Start Date of Week (YYYY-MM-DD):"])
+    start_date = input("Type here: ").strip()
     flights = logic_wrapper.get_flights_by_week_with_manning_info(start_date)
+
+    flight_content = ["Flights for the week starting from " + start_date]
     for flight, is_manned in flights:
-        print(f"Flight {flight.id} on {flight.start_home}: {'Properly Manned' if is_manned else 'Not Properly Manned'}")
+        flight_content.append(f"Flight {flight.id} on {flight.start_home}: {'Properly Manned' if is_manned else 'Not Properly Manned'}")
+
+    interface(flight_content)
+
+    input()
+    return flights_menu()
+
