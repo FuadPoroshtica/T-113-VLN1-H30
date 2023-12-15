b733823303408363f1331f4c5f6f5283f53dae43
Fuad Poroshtica <fuad23@ru.is>
2023-12-15 13:19:37 +0000

    Sechedule UI update

--- Source/ui/employee_schedule.py
+++ Source/ui/employee_schedule.py
@@ -12,10 +12,14 @@ logic_wrapper = Logic_Wrapper(data_wrapper)
 def employee_schedule_menu():
     menu_stack.append(employee_schedule_menu)
     while True:
-        print("\nEmployee Schedule Menu")
-        print("------------------------")
-        print("1. Add Employees to Flights")
-        print("Main Menu (M), Back (B), Quit (Q)")
+        content = [
+            "Employee Schedule Menu",
+            "------------------------------------------------",
+            "1. Add Employees to Flights",
+            "------------------------------------------------",
+            "Main Menu (M), Back (B), Quit (Q)",
+        ]
+        interface(content)
         choice = input("Select Option: ").upper()
 
         if choice == '1':
@@ -27,52 +31,57 @@ def employee_schedule_menu():
             return_to_previous_menu()
             break
         elif choice == "Q":
-            print("Exiting the program.")
+            interface(["Exiting the program."])
             exit()
         else:
-            print("Invalid choice. Please choose again.")
+            interface(["Invalid choice. Please choose again."])
+
 
 def add_employees_to_flight():
     undermanned_flights = logic_wrapper.get_undermanned_flights()
     if not undermanned_flights:
-        print("\nNo undermanned flights found.")
+        interface(["No undermanned flights found."])
         return
 
-    print("\nUndermanned Flights:")
-    for flight in undermanned_flights:
-        print(f"{flight.id}: From {flight.initial_location} to {flight.arrival_location}")
+    flights_content = ["Undermanned Flights:"] + \
+                      [f"{flight.id}: From {flight.initial_location} to {flight.arrival_location}" for flight in undermanned_flights]
+    interface(flights_content)
 
-    flight_id = input("\nEnter Flight ID to add employees: ")
+    interface(["Enter Flight ID to add employees:"])
+    flight_id = input("Type here: ").strip()
     selected_flight = logic_wrapper.get_flight_by_id(flight_id)
 
     if not selected_flight:
-        print("Flight not found.")
+        interface(["Flight not found."])
         return
 
     available_pilots = logic_wrapper.get_available_pilots_for_flight(flight_id)
-    print("\nAvailable Pilots:")
-    for pilot in available_pilots:
-        print(f"{pilot.id}: {pilot.name}")
+    pilots_content = ["Available Pilots:"] + \
+                     [f"{pilot.id}: {pilot.name}" for pilot in available_pilots]
+    interface(pilots_content)
 
     while True:
-        pilot_id = input("Enter Pilot ID to add (or 'done' to finish): ")
-        if pilot_id.lower() == 'done':
+        interface(["Enter Pilot ID to add (or 'done' to finish):"])
+        pilot_id = input("Type here: ").strip().lower()
+        if pilot_id == 'done':
             break
         if logic_wrapper.add_pilot_to_flight(flight_id, pilot_id):
-            print("Pilot added successfully.")
+            interface(["Pilot added successfully."])
         else:
-            print("Invalid Pilot ID or Pilot not available.")
+            interface(["Invalid Pilot ID or Pilot not available."])
 
     available_stewards = logic_wrapper.get_available_stewards_for_flight(flight_id)
-    print("\nAvailable Stewards:")
-    for steward in available_stewards:
-        print(f"{steward.id}: {steward.name}")
+    stewards_content = ["Available Stewards:"] + \
+                       [f"{steward.id}: {steward.name}" for steward in available_stewards]
+    interface(stewards_content)
 
     while True:
-        steward_id = input("Enter Steward ID to add (or 'done' to finish): ")
-        if steward_id.lower() == 'done':
+        interface(["Enter Steward ID to add (or 'done' to finish):"])
+        steward_id = input("Type here: ").strip().lower()
+        if steward_id == 'done':
             break
         if logic_wrapper.add_steward_to_flight(flight_id, steward_id):
-            print("Steward added successfully.")
+            interface(["Steward added successfully."])
         else:
-            print("Invalid Steward ID or Steward not available.")
+            interface(["Invalid Steward ID or Steward not available."])
+
